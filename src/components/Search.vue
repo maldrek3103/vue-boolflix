a95e8e501c4c7eb3ccf37cd5462462883db2498a
maldrek3103 <cpisanu96@gmail.com>
2021-09-14 15:52:21 +0200

    Started milestone 1, searchbar

---
+++ src/components/Search.vue
@@ -0,0 +1,12 @@
+<template>
+  <section><input type="text" /> <button type="submit">Search</button></section>
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<style>
+</style>
