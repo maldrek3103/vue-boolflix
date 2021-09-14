7e569c675682a29e90d6de9bb01d2fd20933388f
maldrek3103 <cpisanu96@gmail.com>
2021-09-14 16:21:30 +0200

    Added api call from input

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
   <div id="app">
     <header>
-      <Search />
+      <Search placeholder="Search a movie/serie" @search="getInput" />
     </header>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import Search from "../src/components/Search.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      movies: [],
+      api: {
+        key: "695b15da881bf459a0f6ee822a62d1d7",
+        base: "https://api.themoviedb.org/3",
+      },
+    };
+  },
   components: {
     Search,
+  },
+  methods: {
+    getInput(query) {
+      if (!query) this.movies = [];
+      const params = {
+        params: {
+          query,
+          api_key: this.api.key,
+          language: "it-IT",
+        },
+      };
+
+      axios.get(`${this.api.base}/search/movie/`, params).then((res) => {
+        this.movies = res.data.results;
+      });
+    },
   },
 };
 </script>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,26 @@
 <template>
-  <section><input type="text" /> <button type="submit">Search</button></section>
+  <section>
+    <input type="text" :placeholder="placeholder" v-model.trim="inputText" />
+    <button type="submit" @click="sendSearch" @keyup.enter="sendSearch">
+      {{ submitText || "Search" }}
+    </button>
+  </section>
 </template>
 
 <script>
 export default {
   name: "Search",
+  props: ["placeholder", "submitText"],
+  data() {
+    return {
+      inputText: "",
+    };
+  },
+  methods: {
+    sendSearch() {
+      this.$emit("search", this.inputText);
+    },
+  },
 };
 </script>
 
